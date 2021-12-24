32e0079e1539164f9d113e77b92cd56584a9ccd0
MutiaMuthmainnah <72694790+MutiaMuthmainnah@users.noreply.github.com>
2021-12-24 23:37:20 +0700

    Mengupload tugas pekan 10 Backend-js

---
+++ task.js
@@ -0,0 +1,33 @@
+/**
+ * Fungsi untuk menampilkan hasil download
+ * @param {string} result - Nama file yang didownload
+ */
+ const showDownload = (result) => {
+    console.log("Download selesai");
+    console.log("Hasil Download: " + result);
+  }
+  
+  /**
+   * Fungsi untuk download file
+   * @param {function} callback - Function callback show
+   */
+  const download = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const result = "windows-10.exe";
+            resolve(result);
+          }, 3000);
+    })
+  }
+  
+  async function main(){
+      showDownload(await download());
+  }
+  
+  main();
+  /**
+   * TODO:
+   * - Refactor callback ke Promise atau Async Await
+   * - Refactor function ke ES6 Arrow Function
+   * - Refactor string ke ES6 Template Literals
+   */
